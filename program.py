cd5cebc38bf2f6d8cffcfae6af512371bf0bef16
Shadmaan <shadmaan@gmail.com>
2018-09-07 13:47:04 -0700

    Initialize program.py

---
+++ program.py
@@ -0,0 +1,30 @@
+""" before you start your app
+    1- create a directory to host the app
+    2- Initiliaze a git repo
+    3- create a readme file explaining what the app is about.
+    4- create a file called program.py which has the code below
+    5- add and commit to git 
+    6- start implementing the functions after each function is completed commit your changes
+    7- have for each stage a different version
+"""
+# Stage 1
+
+
+def main():
+    # print_header() # TODO: just print a pretty header for your app
+    # get_folder_user() # TODO: ask the user to give you the path to the folder
+    # get_keyword_user() # TODO: ask the user to give you the keyword to find
+    # return_results() # TODO: return line_no, the line of every instance found
+
+
+if __name__ == '__main__':
+    main()
+
+# Stage 2
+# once you finish the first stage, then we can move to add argparse so it can run as one command line
+
+# Stage 3
+# add logging capability to the app
+
+# Stage 4
+# add unit testing
